9644ce483f93e1a1b95021ff048f4d8250b14a31
troy prujean <9930285@student.toiohomai.ac.nz>
2018-08-09 18:38:52 +1200

    added list

--- mainMenu/question4/Program.cs
+++ mainMenu/question4/Program.cs
@@ -10,19 +10,38 @@ namespace question4
     {
         static void Main(string[] args)
         {
+            bool newPerson = true;
             Console.WriteLine("Enter name of new person below");
-            Console.Write("First name: ");
-            string userInput1 = Console.ReadLine();
-            Console.Write("Last name: ");
-            string userInput2 = Console.ReadLine();
-            Person user1 = new Person(userInput1, userInput2);
-            Console.WriteLine("\n\n");
-            Console.WriteLine("Creating new person record ...");
-            Console.WriteLine("\n");
-            Console.Write("Enter year of birth: ");
-            user1.YearOfBirth = int.Parse(Console.ReadLine());
-            Console.WriteLine("\n\n");
-            Console.WriteLine($"Person: {user1.FirstName} {user1.LastName} is {user1.GetAge()} years old");
+            var people = new List<Person>();
+            do
+            {
+                Console.Write("First name: ");
+                string userInput1 = Console.ReadLine();
+                Console.Write("Last name: ");
+                string userInput2 = Console.ReadLine();
+                Console.WriteLine("\n\n");
+                Console.WriteLine("Creating new person record ...");
+                Console.WriteLine("\n");
+                Console.Write("Enter year of birth: ");
+                int userInput3 = int.Parse(Console.ReadLine());
+                people.Add(new Person(userInput1, userInput2){YearOfBirth = userInput3});
+                Console.WriteLine("\n\n");
+                Console.Write("Do you want to enter a new person? <y/n>");
+                string userInput4 = Console.ReadLine();
+                if (userInput4 == "y")
+                {
+                    newPerson = true;
+                }
+                else if (userInput4 == "n")
+                {
+                    newPerson = false;
+                }
+            } while (newPerson);
+
+            foreach(Person x in people)
+            {
+                Console.WriteLine($"Person: {x.FirstName} {x.LastName} is {x.GetAge()} years old");
+            }
         }
     }
 }
